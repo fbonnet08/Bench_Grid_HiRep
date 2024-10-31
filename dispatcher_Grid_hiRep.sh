059330b82810e22a73eade50b253d693ee6afb71
fbonnet08 <fbonnet08@gmail.com>
2024-10-31 16:31:25 +0100

    - reorganisation of the dispatcher script to have a git clone instead and reduce the number of ssh commands

--- dispatcher_Grid_hiRep.sh
+++ dispatcher_Grid_hiRep.sh
@@ -9,11 +9,11 @@ sptr="/"
 #echo           "Enter Username         : "; username="frederic"                    # read username;
 #echo           "Enter remote hostname  : "; remote_hostname="137.44.5.215" # read remote_hostname;
 
-#echo           "Enter Username         : "; username="dc-bonn2"                    # read username;
-#echo           "Enter remote hostname  : "; remote_hostname="tursa.dirac.ed.ac.uk" # read remote_hostname;
+echo           "Enter Username         : "; username="dc-bonn2"                    # read username;
+echo           "Enter remote hostname  : "; remote_hostname="tursa.dirac.ed.ac.uk" # read remote_hostname;
 
-echo           "Enter Username         : "; username="eufredericb"                  # read username;
-echo           "Enter remote hostname  : "; remote_hostname="login.vega.izum.si"    # read remote_hostname;
+#echo           "Enter Username         : "; username="eufredericb"                  # read username;
+#echo           "Enter remote hostname  : "; remote_hostname="login.vega.izum.si"    # read remote_hostname;
 
 echo
 $white; printf "username               : ";$green;  printf "$username\n";$reset_colors;
@@ -29,10 +29,13 @@ $white; printf "user remote home dir   : ";$magenta; printf "$user_remote_home_d
 # Setting the external lib_dir
 echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 lib_dir="SwanSea/SourceCodes/external_lib";
+src_dir="SwanSea/SourceCodes"
 #external_lib_dir=$(echo $user_remote_home_dir$sptr$lib_dir | tr -d ' ')
 chopped=$(echo "${user_remote_home_dir}" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
 external_lib_dir=$chopped$sptr$lib_dir
+source_dir=$chopped$sptr$src_dir
 $white; printf "external_lib_dir       : ";$yellow; printf "$external_lib_dir\n";$reset_colors;
+$white; printf "source_dir             : ";$magenta; printf "$source_dir\n";$reset_colors;
 echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 
 ssh -t $user_remote_host "
@@ -40,26 +43,45 @@ ssh -t $user_remote_host "
 red=\"tput setaf 1\"  ;green=\"tput setaf 2\"  ;yellow=\"tput setaf 3\"
 blue=\"tput setaf 4\" ;magenta=\"tput setaf 5\";cyan=\"tput setaf 6\"
 white=\"tput setaf 7\";bold=\"\"               ;reset_colors=\"\"
+
+
+# First let's get the code
+cd ${source_dir} ; pwd ; ls -al ${source_dir}
+git clone https://github.com/fbonnet08/Bench_Grid_HiRep.git
+cd ./Bench_Grid_HiRep.git
+
+
 if [ -d ${external_lib_dir} ]
 then
   \$white; printf \"Directory              : \"; \$bold;
   \$blue; printf \'%s\'\"${external_lib_dir}\"; \$green; printf \" exist, nothing to do.\n\"; \$white; \$reset_colors;
 else
   \$white; printf \"Directory              : \"; \$bold;
-  \$blue; printf \'%s\'\"${external_lib_dir}\"; \$red;printf \" does not exist, We will create it ...\n\"; \$white; \$reset_colors;
+  \$blue; printf \'%s\'\"${external_lib_dir}\"; \$red;printf \" does not exist, We will create it ...\n\";
+   \$white; \$reset_colors;
   mkdir -p ${external_lib_dir}
   printf \"                       : \"; \$bold;
   \$green; printf \"done.\n\"; \$reset_colors;
 fi
+
+# moving files to destination
+cp build_*.sh ${external_lib_dir}
+
+# Now moving to the directory external_dir directory
 cd ${external_lib_dir};
 pwd
 ls -al ${external_lib_dir}
+
+
+
+
+
 "
 
-scp -r ./dependencies_Grid.sh ./Scripts ${user_remote_host}:${external_lib_dir}
+#scp -r ./dependencies_Grid.sh ./Scripts ${user_remote_host}:${external_lib_dir}
 
-ssh -t $user_remote_host " cd ${external_lib_dir};
- ls -al; which bash ; bash -s < ./dependencies_Grid.sh SwanSea/SourceCodes/external_lib;"
+#ssh -t $user_remote_host " cd ${external_lib_dir};
+# ls -al; which bash ; bash -s < ./dependencies_Grid.sh SwanSea/SourceCodes/external_lib;"
 
 
 
