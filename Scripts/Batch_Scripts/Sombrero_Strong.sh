80bb573c506289a926cb29465432252ace29d067
fbonnet08 <fbonnet08@gmail.com>
2024-11-14 10:12:37 +0100

    - [Dev]: internal change for dev

--- Scripts/Batch_Scripts/Sombrero_Strong.sh
+++ Scripts/Batch_Scripts/Sombrero_Strong.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 #
+#SBATCH --nodes=4
 #SBATCH --ntasks=256
+#SBATCH --ntasks-per-node=128
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=cpu
 #SBATCH --job-name=sombrero_strong
 #SBATCH --time=0-0:20
-#SBATCH --ntasks-per-node=32
+#SBATCH --qos=dev
+#-------------------------------------------------------------------------------
+# Getting the common code setup and variables, #setting up the environment properly.
+#-------------------------------------------------------------------------------
+
+source ../../common_main.sh $1;
+
+#-------------------------------------------------------------------------------
+# Run the Sombrero launcher from the src directory
+#-------------------------------------------------------------------------------
+
+echo $sombrero_dir
+echo $LatticeRuns_dir
+
+cd $LatticeRuns_dir
+echo `pwd`
+ls -la
 
 cd $SLURM_SUBMIT_DIR
 
