12fc2d5794be75c4f8b54a30a026101dba1c725c
fbonnet08 <fbonnet08@gmail.com>
2024-11-14 11:46:54 +0100

    - [Dev]: internal change for dev

--- Scripts/Batch_Scripts/Sombrero_Weak.sh
+++ Scripts/Batch_Scripts/Sombrero_Weak.sh
@@ -6,7 +6,7 @@ set -eu
 #SBATCH --partition=cpu
 #SBATCH --job-name=sombrero_weak
 #SBATCH --time=0-0:20
-#SBATCH --qos=dev
+#SBATCH --qos=standard
 #-------------------------------------------------------------------------------
 # Getting the common code setup and variables, #setting up the environment properly.
 #-------------------------------------------------------------------------------
