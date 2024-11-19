8dd52e3a0c35bdc5789bfc3f595f729af638281c
fbonnet08 <fbonnet08@gmail.com>
2024-11-19 16:28:02 +0100

    - [Dev]: internal change for dev

--- Scripts/Batch_Scripts/Compile_BKeeper.sh
+++ Scripts/Batch_Scripts/Compile_BKeeper.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --partition=cpu
 #SBATCH --job-name=complBKeeper
-#SBATCH --time=5:0:0
+#SBATCH --time=15:0:0
 #SBATCH --qos=standard
 #-------------------------------------------------------------------------------
 # Getting the common code setup and variables, #setting up the environment properly.
