6c7ddfea010d3cf98ac524a00ae0270d1f2d955c
fbonnet08 <fbonnet08@gmail.com>
2025-01-28 15:36:41 +0000

    - [Dev]: forgot to update the latest version of submission script job_BKeeper-Leonardo.sh.

--- doc/BKeeper/job_BKeeper-Leonardo.sh
+++ doc/BKeeper/job_BKeeper-Leonardo.sh
@@ -2,20 +2,24 @@
 #SBATCH --job-name=TEST-JOB            # Job name
 #SBATCH --output=log/%x.%j.out
 #SBATCH --error=log/%x.%j.err
-#SBATCH --time=1-12:00:00              # Run time (d-hh:mm:ss)
-#SBATCH --partition=standard-g         # partition name
+#SBATCH --time=00:30:00                # Run time (d-hh:mm:ss)
+#SBATCH --partition=boost_usr_prod
 #SBATCH --nodes=2                      # Total number of nodes
-#SBATCH --ntasks-per-node=8            # 8 MPI ranks per node, 16 total (2x8)
-#SBATCH --gpus-per-node=8              # Allocate one gpu per MPI rank
-#SBATCH --time=1-12:00:00              # Run time (d-hh:mm:ss)
-#SBATCH --account=project_465001614
+#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-socket=4
+#SBATCH --cpus-per-task=8
+#SBATCH --gpus-per-node=4
+#SBATCH --gpus-per-task=1
+#SBATCH --gres=gpu:4
+#SBATCH --mem=494000
+#SBATCH --account=EUHPC_B17_015
 #-------------------------------------------------------------------------------
 # Module loads and compiler version
 #-------------------------------------------------------------------------------
-module load cray-mpich cray-fftw
+module load nvhpc/23.11 fftw/3.3.10--openmpi--4.1.6--gcc--12.2.0 hdf5
 module list;
 
-hipcc --version
+nvcc --version
 #-------------------------------------------------------------------------------
 # Variable expoiorts
 #-------------------------------------------------------------------------------
@@ -37,20 +41,6 @@ benchmark_input_dir=${Bench_Grid_HiRep_dir}/benchmarks
 #-------------------------------------------------------------------------------
 # Getting the gpu select script
 #-------------------------------------------------------------------------------
-cat << EOF > select_gpu
-#!/bin/bash
-
-export ROCR_VISIBLE_DEVICES=\$SLURM_LOCALID
-exec \$*
-EOF
-
-chmod +x ./select_gpu
-
-CPU_BIND="mask_cpu:7e000000000000,7e00000000000000"
-CPU_BIND="${CPU_BIND},7e0000,7e000000"
-CPU_BIND="${CPU_BIND},7e,7e00"
-CPU_BIND="${CPU_BIND},7e00000000,7e0000000000"
-
 export OMP_NUM_THREADS=8
 export MPICH_GPU_SUPPORT_ENABLED=1
 #-------------------------------------------------------------------------------
@@ -69,8 +59,8 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PREFIX_HOME/lib
 #wrapper_script=${Bench_Grid_HiRep_dir}/doc/BKeeper/gpu-mpi-wrapper-new-Lumi.sh
 # run! #########################################################################
 # --mca pml ucx
-srun --cpu-bind=${CPU_BIND} \
-  ./select_gpu "${bkeeper_build_dir}"/BKeeper  \
+srun \
+  "${bkeeper_build_dir}"/BKeeper  \
   "${benchmark_input_dir}"/BKeeper/input_BKeeper.xml \
   --grid 48.48.48.96 \
   --mpi 1.2.2.4 \
@@ -79,14 +69,13 @@ srun --cpu-bind=${CPU_BIND} \
   --device-mem 23000 \
   --log Error,Warning,Message
 ################################################################################
-rm -rf ./select_gpu
 #-------------------------------------------------------------------------------
 #End of the script
 echo
 echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
 echo `date`;
 echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-echo "-                  job_BKeeper-Lumi.sh Done.                            -"
+echo "-                  job_BKeeper-Leonardo.sh Done.                        -"
 echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-# srun --account=project_465001614 --partition=dev-g --time=00:30:00 --nodes=1 --gres=gpu:8 --pty bash
+# srun --account=EUHPC_B17_015 --partition=boost_usr_prod --time=00:30:00 --nodes=1 --gres=gpu:4 --pty bash
 #-------------------------------------------------------------------------------
