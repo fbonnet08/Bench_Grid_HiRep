8270f67f96e870b573b70c08e66fc76a901ad9c8
fbonnet08 <fbonnet08@gmail.com>
2025-02-12 17:10:57 +0000

    - GPU Wrapper fixe for the Vega system.

--- doc/BKeeper/gpu-mpi-wrapper-new-Vega.sh
+++ doc/BKeeper/gpu-mpi-wrapper-new-Vega.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
-
 lrank=$OMPI_COMM_WORLD_LOCAL_RANK
-numa1=$((lrank))
-netdev=mlx5_${lrank}:1
+#numa1=$((lrank))
+#netdev=mlx5_${lrank}:1
+numa1=$(( 3 + 4 * (lrank / 2) - 2 * (lrank % 2) ))
+numa2=$(( numa1-1 ))
+netdev=$(( lrank / 2 ))
+
+echo "lrank        --->: $lrank"
+echo "numa1        --->: $numa1"
+echo "numa2        --->: $numa2"
+echo "netdev       --->: $netdev"
 
 export CUDA_VISIBLE_DEVICES=$OMPI_COMM_WORLD_LOCAL_RANK
 export UCX_NET_DEVICES=${netdev}
-BINDING="--interleave=$numa1"
+BINDING="--interleave=${numa1},${numa2}"
+
+echo "BINDING      --->: $BINDING"
+echo "numa command --->: numactl ${BINDING} $@"
 
 echo "$(hostname) - $lrank device=$CUDA_VISIBLE_DEVICES binding=$BINDING"
 
