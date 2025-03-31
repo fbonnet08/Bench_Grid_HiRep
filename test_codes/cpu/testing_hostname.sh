229f5288c4a64bc67123e6f98c56658b43d97693
frede <fbonnet08@gmail.com>
2025-03-31 13:16:36 +0100

    - [Dev]: little test code

---
+++ test_codes/cpu/testing_hostname.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+#colors
+red="tput setaf 1"  ;green="tput setaf 2"  ;yellow="tput setaf 3"
+blue="tput setaf 4" ;magenta="tput setaf 5";cyan="tput setaf 6"
+white="tput setaf 7";bold=""               ;reset_colors="tput sgr0"
+
+hostname=$(echo $HOSTNAME); echo $hostname
+
+if [[ $hostname =~ "tursa" ]]; then
+  echo "Found 'tursa' in string."
+  machine="tursa"
+fi
+
+echo "$hostname"
+echo "$machine"
+
+echo `pwd`
+#-------------------------------------------------------------------------------
+#End of the script
+echo
+echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+$cyan; echo `date`; $blue;
+echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+echo "-                  testing_hostname.sh Done.                            -"
+echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
+$reset_colors
+#exit
+#-------------------------------------------------------------------------------
