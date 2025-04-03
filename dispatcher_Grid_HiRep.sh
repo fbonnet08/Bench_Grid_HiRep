91fe2954b89e64c65d255244ebb895c041a08a21
frede <fbonnet08@gmail.com>
2025-04-03 14:50:57 +0100

    - [Dev]: Inserted the launcher_bench_Grid-DWF-Telos.sh into the dispatcher_Grid_HiRep.sh.

--- dispatcher_Grid_HiRep.sh
+++ dispatcher_Grid_HiRep.sh
@@ -233,6 +233,8 @@ bash -s < ./build_SombreroBKeeper.sh           SwanSea/SourceCodes/external_lib;
 
 #bash -s < ./launcher_bench_BKeeper.sh         SwanSea/SourceCodes/external_lib BKeeper_run_gpu
 
+bash -s < ./launcher_bench_Grid-DWF-Telos.sh   SwanSea/SourceCodes/external_lib Grid_DWF_run_gpu
+
 #bash -s < ./launcher_bench_Sombrero.sh        SwanSea/SourceCodes/external_lib Sombrero_weak;
 #bash -s < ./launcher_bench_Sombrero.sh        SwanSea/SourceCodes/external_lib Sombrero_strong;
 #bash -s < ./launcher_bench_Grid.sh            SwanSea/SourceCodes/external_lib; -->: Here it will be: Grid_DWF_run_gpu
