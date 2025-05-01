40c94ab5ccb50a512562cf4d6edb7b4f2d27d307
frede <fbonnet08@gmail.com>
2025-05-01 12:59:41 +0100

    ---- Bash ---- - [Update]: Module update for Vega . --------------

--- dispatcher_Grid_HiRep.sh
+++ dispatcher_Grid_HiRep.sh
@@ -170,11 +170,14 @@ case ${remote_hostname} in
     ;;
   *\"login.vega.izum.si\"*)
     source /etc/profile.d/modules.sh;
-    source /ceph/hpc/software/cvmfs_env.sh ;
+    source /ceph/hpc/software/cvmfs_env.sh;
     module list;
-    module load CUDA/12.3.0 OpenMPI/4.1.5-GCC-12.3.0 UCX/1.15.0-GCCcore-12.3.0 GCC/12.3.0;
-    module load FFTW/3.3.10-GCC-12.3.0;
-    module load Python;
+    module load UCX/1.8.0-GCCcore-9.3.0;
+    module load OpenMPI/4.0.3-GCC-9.3.0;
+    module load CUDA/11.0.2-GCC-9.3.0;
+    module load GCC/9.3.0;
+    module load Python/3.8.2-GCCcore-9.3.0;
+    module load FFTW;
     module list;
     ;;
   *\"lumi.csc.fi\"*)
