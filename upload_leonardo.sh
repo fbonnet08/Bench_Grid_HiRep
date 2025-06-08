73958426bb9e41996bd9e2ec994a969e1979ec52
frede <fbonnet08@gmail.com>
2025-06-08 14:03:50 +0100

    ---- Bash ---- - [Update]: upload_leonardo.sh. --------------

--- upload_leonardo.sh
+++ upload_leonardo.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+if [ -f ~/.ssh/known_hosts ]; then rm ~/.ssh/known_hosts; fi
+
 step ca bootstrap --ca-url=https://sshproxy.hpc.cineca.it --fingerprint 2ae1543202304d3f434bdc1a2c92eff2cd2b02110206ef06317e70c1c1735ecd
 
 step ssh login 'fbonnet08@gmail.com' --provisioner cineca-hpc
@@ -9,4 +11,5 @@ eval "$(ssh-agent)"
 step ssh login 'fbonnet08@gmail.com' --provisioner cineca-hpc
 step ssh list --raw 'fbonnet08@gmail.com' | step ssh inspect
 
-scp -r ~/SwanSea/SourceCodes/LLR-Tests/setup_LLR_tests_Leonardo fbonnet0@login.leonardo.cineca.it:~/Downloads/
+scp -r ~/data fbonnet0@login.leonardo.cineca.it:~/
+
