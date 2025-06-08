b19e8e51c4dcc12103ce70543a8b35eeb1fbc219
frede <fbonnet08@gmail.com>
2025-06-08 13:08:59 +0100

    ---- Bash ---- - [Update]: update of the deploy_withXTerm_to_clusters.sh for simultaneous and multiple cluster deployment. --------------

--- deploy_withXTerm_to_clusters.sh
+++ deploy_withXTerm_to_clusters.sh
@@ -24,7 +24,7 @@ sptr="/"
 #-------------------------------------------------------------------------------
 # [Precision-3571]
 #-------------------------------------------------------------------------------
-xterm -bg black -fg white -cr red -geometry 120x70  -e "sh ./dispatcher_Grid_hiRep.sh  frederic     notneeded  137.44.5.215" &
+#xterm -bg black -fg white -cr red -geometry 120x70  -e "sh ./dispatcher_Grid_hiRep.sh  frederic     notneeded  137.44.5.215" &
 #-------------------------------------------------------------------------------
 # [Sunbird] [scw1813, scw1019] TODO: Sunbird has issues with max_string_array
 #-------------------------------------------------------------------------------
@@ -36,34 +36,42 @@ xterm -bg black -fg white -cr red -geometry 120x70  -e "sh ./dispatcher_Grid_hiR
 #-------------------------------------------------------------------------------
 # [Lumi]
 #-------------------------------------------------------------------------------
-xterm -bg black -fg white -cr red -geometry 120x70  -e "sh ./dispatcher_Grid_hiRep.sh  bonnetfr  project_465001614   lumi.csc.fi" &
+#xterm -bg black -fg white -cr red -geometry 120x70  -e "sh ./dispatcher_Grid_hiRep.sh  bonnetfr  project_465001614   lumi.csc.fi" &
 #-------------------------------------------------------------------------------
 # [Tursa]
 #-------------------------------------------------------------------------------
-xterm -bg black -fg white -cr red -geometry 120x70  -e "sh ./dispatcher_Grid_hiRep.sh  dc-bonn2  dp208  tursa.dirac.ed.ac.uk" &
+#xterm -bg black -fg white -cr red -geometry 120x70  -e "sh ./dispatcher_Grid_hiRep.sh  dc-bonn2  dp208  tursa.dirac.ed.ac.uk" &
 #-------------------------------------------------------------------------------
 # [Mi300]
 #-------------------------------------------------------------------------------
-xterm -bg black -fg white -cr red -geometry 120x70  -e "sh ./dispatcher_Grid_HiRep.sh  dc-bonn2  not_required  aac6.amd.com" &
+#xterm -bg black -fg white -cr red -geometry 120x70  -e "sh ./dispatcher_Grid_HiRep.sh  dc-bonn2  not_required  aac6.amd.com" &
 #-------------------------------------------------------------------------------
 # [Mi210]
 #-------------------------------------------------------------------------------
-xterm -bg black -fg white -cr red -geometry 120x70  -e "sh ./dispatcher_Grid_HiRep.sh  dc-bonn2  not_required  aac1.amd.com" &
+#xterm -bg black -fg white -cr red -geometry 120x70  -e "sh ./dispatcher_Grid_HiRep.sh  dc-bonn2  not_required  aac1.amd.com" &
 #-------------------------------------------------------------------------------
 # [Marenostrum]: Authentication  procedure.
 #-------------------------------------------------------------------------------
-# TODO:
+# GPU-Node
+#xterm -bg black -fg white -cr red -geometry 120x70  -e "sh ./dispatcher_Grid_HiRep.sh  swan127136  ehpc191  alogin1.bsc.es" &
+# # CPU-Node comon file system with the gpu node
+#xterm -bg black -fg white -cr red -geometry 120x70  -e "sh ./dispatcher_Grid_HiRep.sh  swan127136  ehpc191  glogin1.bsc.es" &
 #-------------------------------------------------------------------------------
 # [Leonardo]: Authentication  procedure.
 #-------------------------------------------------------------------------------
-: '
+# Booster [GPU]: EUHPC_B17_015     EUHPC_B22_046
+#sh ./dispatcher_Grid_HiRep.sh  fbonnet0  EUHPC_B22_046  login.leonardo.cineca.it
+# DCPG    [CPU]: EUHPC_B17_015_0   EUHPC_B22_046_0
+#sh ./dispatcher_Grid_HiRep.sh  fbonnet0  EUHPC_B22_046_0  login.leonardo.cineca.it
+
+#: '
 step ca bootstrap --ca-url=https://sshproxy.hpc.cineca.it --fingerprint 2ae1543202304d3f434bdc1a2c92eff2cd2b02110206ef06317e70c1c1735ecd;
 step ssh login 'fbonnet08@gmail.com' --provisioner cineca-hpc;
 eval "$(ssh-agent)";
 step ssh login 'fbonnet08@gmail.com' --provisioner cineca-hpc;
 step ssh list --raw 'fbonnet08@gmail.com' | step ssh inspect;
-sh ./dispatcher_Grid_hiRep.sh  fbonnet0  EUHPC_B17_015  login.leonardo.cineca.it &
-'
+sh ./dispatcher_Grid_hiRep.sh  fbonnet0  EUHPC_B22_046_0  login.leonardo.cineca.it &
+#'
 
 #-------------------------------------------------------------------------------
 #End of the script
